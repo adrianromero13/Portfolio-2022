57237446f134f694776a3597618cd871c803d02f
Adrian Romero <radrian031613@gmail.com>
2022-04-22 11:46:27 -0700

    init commit

---
+++ src/components/App/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BroswerRouter as Router, Route } from 'react-router-dom';
+
+// import components needed
+
+const App = props => (
+  <Router>
+    Hello
+  </Router>
+)
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// import components to render
+import App from "./components/App";
+
+// start app
+ReactDOM.render([<App key="1" />], document.getElementById("root"));
