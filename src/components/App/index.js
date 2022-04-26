063ea88f1c74354d0590942f219beb6e6ad4e8f3
Adrian Romero <radrian031613@gmail.com>
2022-04-25 18:37:39 -0700

    fixed semantic rendering issue

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css'
 
 // import components needed
 import Layout from '../Layout';
