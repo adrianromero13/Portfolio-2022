e002a539f4fdecb987b706be091404a973e071fa
Adrian Romero <radrian031613@gmail.com>
2022-04-27 16:55:07 -0700

    added menu items to navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,21 +3,42 @@ import React, { useState } from "react";
 // components needed
 
 const CustomHeader = () => {
-  const [aciveItem, setAciveItem] = useState("home");
+
   return (
-    <nav className="navbar navbar-light bg-light justify-content-between">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
       <a className="navbar-brand">Logo</a>
-      <form className="form-inline">
-        <input
-          className="form-control mr-sm-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
+      <div className="inline ">
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown link
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
         <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-          Search
+          Portfolio
         </button>
-      </form>
+    </ul>
+  </div>
+      </div>
     </nav>
   );
 };
