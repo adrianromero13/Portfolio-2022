8074f7e8da3ea6afbad99b45192f6b3b394358fc
Adrian Romero <radrian031613@gmail.com>
2022-04-27 17:02:17 -0700

    changed some styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 const CustomHeader = () => {
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+    <nav className="navbar navbar-expand-lg navbar-dark well justify-content-between">
       <a className="navbar-brand">Logo</a>
       <div className="inline ">
         
@@ -14,28 +14,20 @@ const CustomHeader = () => {
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
-      <li class="nav-item active">
+      <li class="nav-item">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">About</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link" href="#">Contact</a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
+      <li class="nav-item">
         <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
           Portfolio
         </button>
+      </li>
     </ul>
   </div>
       </div>
