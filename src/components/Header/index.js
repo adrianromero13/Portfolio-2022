3e2d237226dde26ef78461c8daf7cedd7fec392d
Adrian Romero <radrian031613@gmail.com>
2022-04-29 14:33:45 -0700

    changed styling and added main body container

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,33 +3,48 @@ import React, { useState } from "react";
 // components needed
 
 const CustomHeader = () => {
-
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark well justify-content-between">
+    <nav className="navbar navbar-expand-lg navbar-dark myHeader justify-content-between">
       <a className="navbar-brand">Logo</a>
       <div className="inline ">
-        
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contact</a>
-      </li>
-      <li class="nav-item">
-        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
-          Portfolio
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
         </button>
-      </li>
-    </ul>
-  </div>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Home <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                About
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Contact
+              </a>
+            </li>
+            <li class="nav-item">
+              <button
+                className="btn btn-outline-success my-2 my-sm-0"
+                type="submit"
+              >
+                Portfolio
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
